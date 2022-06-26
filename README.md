b87d2c88f9da77e145d5f00b46a89408febd2655
roger10casssares <roger10casssares@gmail.com>
2022-06-25 22:33:08 -0300

    First COmmit

---
+++ README.md
@@ -0,0 +1,5 @@
+# rocketseat-ignite-lab-02-event-platform
+
+Create svg to a react component
+https://svg2jsx.com/
+
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { gql, useQuery } from "@apollo/client";
+import { client } from "./lib/apollo";
+import { Event } from "../pages/Event";
+import { Router } from "./Router";
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+
+export function App() {
+  return (
+    <div>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
+    </div>
+  );
+}
